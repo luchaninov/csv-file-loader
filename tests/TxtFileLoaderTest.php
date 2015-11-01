7859ffb5ccf7c8948be5e8d53f9b9b3b7ed9633d
luchaninov <vladimir@luchaninov.com>
2015-11-01 15:24:56 +0200

    test for TxtFileLoader->setSkipComments

--- tests/TxtFileLoaderTest.php
+++ tests/TxtFileLoaderTest.php
@@ -40,4 +40,23 @@ class TxtFileLoaderTest extends \PHPUnit_Framework_TestCase
 
         @unlink($filename);
     }
+
+    public function testGetItemsSkippingComments()
+    {
+        $filename = sys_get_temp_dir() . '/test_CsvFileLoader_' . microtime(true) . '.txt';
+        file_put_contents($filename, implode("\n", ['test1', 'test2', '# comment', 'test3', '  # comment with whitespaces']));
+
+        $loader = new TxtFileLoader($filename);
+        $loader->setSkipComments(true);
+
+        $actual = [];
+        foreach ($loader->getItems() as $item) {
+            $actual[] = $item;
+        }
+
+        $expected = ['test1', 'test2', 'test3'];
+        $this->assertEquals($expected, $actual);
+
+        @unlink($filename);
+    }
 }
